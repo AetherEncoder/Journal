60372b430790012d1be9d9556dbf9a00fd97fbd7
AetherEncoder <saikopewds@gmail.com>
2024-11-12 23:04:44 +0800

    Fresh start

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
-    const dimmedBackground = document.querySelector('.dimmed-background');
-  
-    cards.forEach(card => {
-      card.addEventListener('click', () => {
-        card.classList.toggle('flipped');
-        dimmedBackground.classList.toggle('active');
-      });
-    });
-  
-    dimmedBackground.addEventListener('click', () => {
-      cards.forEach(card => card.classList.remove('flipped'));
-      dimmedBackground.classList.remove('active');
+gsap.fromTo(
+  ".loading-page", 
+  {opacity: 1}, 
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+    onComplete: function() {
+      document.documentElement.classList.remove('loading');
+      document.body.classList.remove('loading');
+    }
+  }
+);
+
+// Add event listener to the "HELLO" button
+document.getElementById('move-button-right').addEventListener('click', function() {
+  gsap.to('.deck', { x: '+=400', duration: 1 });
+});
+
+document.getElementById('move-button-left').addEventListener('click', function() {
+  gsap.to('.deck', { x: '-=400', duration: 1 });
+});
+
+document.querySelectorAll('.navigation-container a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
     });
   });
+});
